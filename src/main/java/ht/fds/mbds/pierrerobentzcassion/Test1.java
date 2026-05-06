643b2836ece04e7aef3b5d92d8f2166603f93a10
thecassion <pierrerobentz.cassion@gmail.com>
2026-05-06 09:24:00 -0400

    Test1 : Verification qu'un llm est sans etat.

--- src/main/java/ht/fds/mbds/pierrerobentzcassion/Test1.java
+++ src/main/java/ht/fds/mbds/pierrerobentzcassion/Test1.java
@@ -1,4 +1,23 @@
 package ht.fds.mbds.pierrerobentzcassion;
 
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+
 public class Test1 {
+    public static void main(String[] args) {
+        String cle= System.getenv("GEMINI_KEY");
+        // Création du modèle avec un builder
+        ChatModel modele = GoogleAiGeminiChatModel.builder() .apiKey(cle)
+                .modelName("gemini-2.5-flash")
+                .temperature(0.7)
+                .build();
+        // Pose une question au modèle
+        String reponse= modele.chat("Bonjour je m'appelle Pierre Robentz Cassion");
+        // Affiche la réponse du modèle (hello)
+        System.out.println(reponse);
+
+        String response2 = modele.chat("Quel est mon nom");
+
+        System.out.println(response2);
+    }
 }
