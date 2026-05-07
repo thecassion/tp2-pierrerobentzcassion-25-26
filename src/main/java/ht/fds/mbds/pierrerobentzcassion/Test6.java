2054906c2561d4ba2e07648a39e6b1302dd4c23f
thecassion <pierrerobentz.cassion@gmail.com>
2026-05-06 21:52:27 -0400

    Test 6 : RAG 2

---
+++ src/main/java/ht/fds/mbds/pierrerobentzcassion/Test6.java
@@ -0,0 +1,85 @@
+package ht.fds.mbds.pierrerobentzcassion;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+
+import java.util.Scanner;
+
+/**
+ * Le RAG facile !
+ */
+public class Test6 {
+
+    // Assistant conversationnel
+    interface Assistant {
+        // Prend un message de l'utilisateur et retourne une réponse du LLM.
+        String chat(String userMessage);
+    }
+
+    public static void main(String[] args) {
+        String llmKey = System.getenv("GEMINI_KEY");
+        if (llmKey == null) {
+            System.out.println("La variable d'environnement GEMINI_KEY n'est pas définie.");
+            return;
+        }
+
+        // Mettre une température qui ne dépasse pas 0,3.
+        // Le RAG sert à mieux contrôler l'exactitude des informations données par le LLM
+        // et il est donc logique de diminuer la température.
+        ChatModel model = GoogleAiGeminiChatModel.builder() .apiKey(llmKey)
+                .modelName("gemini-2.5-flash")
+                .temperature(0.3)
+                .build();
+
+        // Chargement du document, sous la forme d'embeddings, dans une base vectorielle en mémoire
+        String nomDocument = "ml.pdf";
+        Document document = FileSystemDocumentLoader.loadDocument(nomDocument);
+        EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        // Calcule les embeddings et les enregistre dans la base vectorielle
+        EmbeddingStoreIngestor.ingest(document, embeddingStore);
+
+        // Création de l'assistant conversationnel, avec une mémoire.
+        // L'implémentation de Assistant est faite par LangChain4j.
+        // L'assistant gardera en mémoire les 10 derniers messages.
+        // La base vectorielle en mémoire est utilisée pour retrouver les embeddings.
+        Assistant assistant =
+                AiServices.builder(Assistant.class)
+                        .chatModel(model)
+                        .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+                        .contentRetriever(EmbeddingStoreContentRetriever.from(embeddingStore))
+                        .build();
+
+
+        conversationAvec(assistant);
+    }
+
+    private static void conversationAvec(Assistant assistant) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("==================================================");
+                System.out.println("Posez votre question : ");
+                String question = scanner.nextLine();
+                if (question.isBlank()) {
+                    continue;
+                }
+                System.out.println("==================================================");
+                if ("fin".equalsIgnoreCase(question)) {
+                    break;
+                }
+                String reponse = assistant.chat(question);
+                System.out.println("Assistant : " + reponse);
+                System.out.println("==================================================");
+            }
+        }
+    }
+
+}
